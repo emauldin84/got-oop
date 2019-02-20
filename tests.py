1445edd50767cb4c26ef68d3c0aeff095a97ad1e
Eric Mauldin <emauldin84@gmail.com>
2019-02-20 11:31:28 -0500

    adds inventory to character class

--- tests.py
+++ tests.py
@@ -9,3 +9,10 @@ jon = Character("Jon Snow", "jon.png")
 
 print(arya.name, arya.avatar)
 print(jon.name, jon.avatar)
+
+# After two items to inventory, length of inventory should == 2
+
+arya.inventory.append('Needle')
+arya.inventory.append('mask')
+
+print("Arya has %d items in her inventory" % len(arya.inventory))
